724875117c149c021912f8ba74e6f116584d1125
Joe Marzullo <jmarzu13@gmail.com>
2016-06-22 22:18:14 -0700

    Front page, ingredients search, recipies landing page set up and paths work

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.send('What\'s in your fridge?');
+  // res.send('What\'s in your fridge?');
+  res.render('mainPage');
 });
+
+app.get('/mainPage', function(req, res) {
+  // res.send('the button worked');
+  res.render('searchFridge');
+});
+
+app.get('/searchFridge', function(req, res) {
+  res.render('recipies');
+})
 
 var server = app.listen(process.env.PORT || 3000);
 
