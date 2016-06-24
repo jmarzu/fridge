e3c19c9651502b0eb928fbcfc13e23114e90993d
Joe Marzullo <jmarzu13@gmail.com>
2016-06-23 17:38:57 -0700

    Just need the profile tests to run

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser')
 var ejsLayouts = require('express-ejs-layouts');
+var session = require('express-session');
+var passport = require('./config/ppConfig');
+var isLoggedIn = require('./middleware/isLoggedIn');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 app.use(express.static('public'));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res) {
   res.render('mainPage');
@@ -17,7 +27,12 @@ app.get('/mainPage', function(req, res) {
   res.render('searchFridge');
 });
 
+app.get('/profile', isLoggedIn, function(req, res) {
+  res.render('profile');
+});
+
 app.use('/searchFridge', require('./controllers/searchFridge'));
+app.use('/auth', require('./controllers/auth'));
 
 var server = app.listen(process.env.PORT || 3000);
 
